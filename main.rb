e59c0a7a573cb0b416496a2572a8c31141a424c9
vagrant <liao098@gmail.com>
2020-03-30 18:28:08 +0000

    initial commit

---
+++ main.rb
@@ -0,0 +1,13 @@
+require './players.rb'
+require './questions.rb'
+require './turns.rb'
+
+
+def game
+  def start
+    puts "New Game!"
+    
+  end
+end
+
+game.start
---
+++ players.rb
@@ -0,0 +1,7 @@
+class Players
+  def initialize(name)
+    @name = name
+    @life = 3
+  end
+  attr_accessor :life
+end
